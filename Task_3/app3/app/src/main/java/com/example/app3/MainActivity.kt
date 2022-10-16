4949148d2d58fc03b08956e5c7de883fd3179768
Anzhela <anpudovaa@yandex.ru>
2022-10-16 03:28:55 +0300

    commit gradle

--- Task_3/app3/app/src/main/java/com/example/app3/MainActivity.kt
+++ Task_3/app3/app/src/main/java/com/example/app3/MainActivity.kt
@@ -4,8 +4,17 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
 class MainActivity : AppCompatActivity() {
+    private var _binding: ActivityMainBinding? = null
+    private val binding get() = _binding!!
+    private val fragmentsContainerId: Int = R.id.fragments_container
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        _binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(R.layout.activity_main)
+
+        supportFragmentManager.beginTransaction()
+            .add(fragmentsContainerId, FirstFragment.getInstance(), FirstFragment.FIRST_FRAGMENT_TAG)
+            .commit()
     }
 }
